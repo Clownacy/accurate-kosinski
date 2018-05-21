5abec14d5d28df82533de7beb485dd778ba1a817
Clownacy <Clownacy@users.noreply.github.com>
2018-05-21 22:06:05 +0100

    Added a sanity check for Moduled Kosinski compressor

--- lib/kosinski_moduled_compress.c
+++ lib/kosinski_moduled_compress.c
@@ -10,30 +10,37 @@
 
 size_t KosinskiCompressModuled(unsigned char *file_buffer, size_t file_size, unsigned char **p_output_buffer)
 {
-	MemoryStream *output_stream = MemoryStream_Init(0x100);
-
-	MemoryStream_WriteByte(output_stream, file_size >> 8);
-	MemoryStream_WriteByte(output_stream, file_size & 0xFF);
-
-	const unsigned int extra_module_count = (file_size - 1) >> 12;
-
-	for (unsigned int i = 0; i < extra_module_count; ++i)
+	if (file_size > 0xFFFF)
 	{
-		unsigned char *compressed_buffer;
-		size_t compressed_size = KosinskiCompress(file_buffer, 0x1000, &compressed_buffer);
-		MemoryStream_WriteBytes(output_stream, compressed_buffer, compressed_size);
-		file_buffer += 0x1000;
+		return 0;	// Cannot fit size of file in header - give up
 	}
+	else
+	{
+		MemoryStream *output_stream = MemoryStream_Init(0x100);
 
-	unsigned char *compressed_buffer;
-	size_t compressed_size = KosinskiCompress(file_buffer, ((file_size - 1) & 0xFFF) + 1, &compressed_buffer);
-	MemoryStream_WriteBytes(output_stream, compressed_buffer, compressed_size);
+		MemoryStream_WriteByte(output_stream, file_size >> 8);
+		MemoryStream_WriteByte(output_stream, file_size & 0xFF);
 
-	size_t output_buffer_size = MemoryStream_GetIndex(output_stream);
-	unsigned char *output_buffer = MemoryStream_GetBuffer(output_stream);
+		const unsigned int extra_module_count = (file_size - 1) >> 12;
 
-	free(output_stream);
+		for (unsigned int i = 0; i < extra_module_count; ++i)
+		{
+			unsigned char *compressed_buffer;
+			size_t compressed_size = KosinskiCompress(file_buffer, 0x1000, &compressed_buffer);
+			MemoryStream_WriteBytes(output_stream, compressed_buffer, compressed_size);
+			file_buffer += 0x1000;
+		}
 
-	*p_output_buffer = output_buffer;
-	return output_buffer_size;
+		unsigned char *compressed_buffer;
+		size_t compressed_size = KosinskiCompress(file_buffer, ((file_size - 1) & 0xFFF) + 1, &compressed_buffer);
+		MemoryStream_WriteBytes(output_stream, compressed_buffer, compressed_size);
+
+		size_t output_buffer_size = MemoryStream_GetIndex(output_stream);
+		unsigned char *output_buffer = MemoryStream_GetBuffer(output_stream);
+
+		free(output_stream);
+
+		*p_output_buffer = output_buffer;
+		return output_buffer_size;
+	}
 }
