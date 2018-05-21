db51bf0f254e5bc99c5a6d5c4b483b4e7bcf5a57
Clownacy <Clownacy@users.noreply.github.com>
2018-05-21 23:35:24 +0100

    Added special compressor that outputs an ASM file
    According to a partial ASM file found in the Sonic 2 Simon Wai
    prototype, Sega's compressor may have output ASM files instead of plain
    old binary files.
    
    https://tcrf.net/Proto:Sonic_the_Hedgehog_2_(Genesis)/Simon_Wai_Prototype#Uncompiled_Data

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
 CFLAGS := -O3 -s -static -std=c11 -fomit-frame-pointer -fno-ident -Wall -Wextra -Wno-maybe-uninitialized
 
-all: compare.exe kosinski_compress.exe kosinski_decompress.exe kosinski_compress_d.exe kosinski_decompress_d.exe kosinskim_compress_d.exe kosinskim_decompress_d.exe kosinskim_compare.exe
+all: compare.exe kosinski_compress.exe kosinski_compress_asm.exe kosinski_decompress.exe kosinski_compress_d.exe kosinski_decompress_d.exe kosinskim_compress_d.exe kosinskim_decompress_d.exe kosinskim_compare.exe
 
 compare.exe: main_compare.c lib/kosinski_decompress.c lib/kosinski_compress.c load_file_to_buffer.c lib/memory_stream.c
 	$(CC) $(CFLAGS) -o $@ $^ $(LIBS)
 
 kosinski_compress.exe: main_compress.c lib/kosinski_compress.c load_file_to_buffer.c lib/memory_stream.c
+	$(CC) $(CFLAGS) -o $@ $^ $(LIBS)
+
+kosinski_compress_asm.exe: main_compress_asm.c lib/kosinski_compress.c load_file_to_buffer.c lib/memory_stream.c
 	$(CC) $(CFLAGS) -o $@ $^ $(LIBS)
 
 kosinski_decompress.exe: main_decompress.c lib/kosinski_decompress.c load_file_to_buffer.c lib/memory_stream.c
---
+++ main_compress_asm.c
@@ -0,0 +1,80 @@
+// Copyright (c) 2018 Clownacy
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "lib/kosinski_compress.h"
+
+#include "load_file_to_buffer.h"
+
+int main(int argc, char *argv[])
+{
+	int success = EXIT_FAILURE;
+
+	if (argc < 2)
+	{
+		printf(
+		"This tool compresses a supplied file in the Kosinski format. It tries to produce files accurate to Sega's original compressor.\n"
+		"Made by Clownacy.\n"
+		"\n"
+		"Usage: kosinski_compress.exe [in_file] [out_file](optional)\n"
+		);
+	}
+	else
+	{
+
+		unsigned char *file_buffer;
+		long int file_size;
+
+		if (LoadFileToBuffer(argv[1], &file_buffer, &file_size))
+		{
+			#ifdef DEBUG
+			printf("File '%s' with size %lX loaded\n", argv[1], file_size);
+			#endif
+
+			unsigned char *out_buffer;
+			size_t out_size = KosinskiCompress(file_buffer, file_size, &out_buffer);
+
+			char *out_filename = (argc > 2) ? argv[2] : "out.asm";
+
+			FILE *out_file = fopen(out_filename, "w");
+
+			if (out_file)
+			{
+				size_t claimed_out_size = (out_size + 0x100) & ~0xFF;
+				fprintf(out_file, "; à≥èkëO $%lx  à≥èkå„ $%x  à≥èkó¶ %.1f%%  ÉZÉãêî %ld", file_size, claimed_out_size, ((float)claimed_out_size / file_size) * 100, file_size / 32);
+
+				unsigned int index = 0;
+				for (unsigned int bytes_remaining = out_size; bytes_remaining != 0; bytes_remaining -= 0x10)
+				{
+					fprintf(out_file, "\n	dc.b	");
+
+					fprintf(out_file, "$%.2x", out_buffer[index++]);
+
+					for (unsigned int i = 1; i < 0x10; ++i)
+					{
+						fprintf(out_file, ",$%.2x", out_buffer[index++]);
+					}
+				}
+
+				free(out_buffer);
+				fclose(out_file);
+
+				success = EXIT_SUCCESS;
+			}
+			else
+			{
+				printf("Could not open '%s'\n", out_filename);
+			}
+
+			free(file_buffer);
+		}
+		else
+		{
+			printf("Could not open '%s'\n", argv[1]);
+		}
+	}
+
+	return success;
+}
